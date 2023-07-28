92f4e77efde3fad5b961ec5c4ac6942616894669
Saagar2005 <118495708+Saagar2005@users.noreply.github.com>
2023-07-28 22:32:08 +0530

    Update Election.sol

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,110 +1,86 @@
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.18;
 
-contract GymkhanaElection {
-    struct Candidate {
-        string name;
-        uint256 voteCount;
-        address candidateAddress;
-    }
-
+contract Election {
     struct Voter {
+        bool voted; // if true, this voter already voted
         bool registered;
-        bool voted;
-        address[] preferences;
     }
 
-    address public owner;
-    uint256 public registrationDeadline;
-    uint256 public votingDeadline;
-    uint256 public totalVotes;
-    Candidate[] public candidates;
+    struct Candidate {
+        string name; // short name (up to 32 bytes)
+        uint256 score; // total score
+        address id;
+    }
+
+    address public leader;
+
+    // This declares a state variable that
+    // maps a `Voter` to each possible address.
     mapping(address => Voter) public voters;
 
-    event CandidateRegistered(string name);
-    event VoterRegistered(address voter);
-    event VoteCast(address voter, address[] preferences);
-    event ElectionResult(string winner, uint256 voteCount);
+    Candidate[] public candidates;
 
-    constructor(uint256 _registrationDeadline, uint256 _votingDeadline) {
-        owner = msg.sender;
-        registrationDeadline = _registrationDeadline;
-        votingDeadline = _votingDeadline;
+    constructor() {
+        leader = msg.sender;
+    }
+    function regVoter() external
+    {
+         Voter storage voter = voters[msg.sender];
+         require(voter.registered == false, "Voter is already registered");
+         voter.registered = true;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can call this function");
-        _;
-    }
-
-    modifier onlyRegisteredVoter() {
-        require(voters[msg.sender].registered, "Only registered voter can call this function");
-        _;
-    }
-
-    modifier beforeRegistrationDeadline() {
-        require(block.timestamp < registrationDeadline, "Registration deadline has passed");
-        _;
-    }
-
-    modifier beforeVotingDeadline() {
-        require(block.timestamp < votingDeadline, "Voting deadline has passed");
-        _;
-    }
-
-    function registerCandidate(string memory _name, address _candidateAddress) public onlyOwner beforeRegistrationDeadline {
-        candidates.push(Candidate(_name, 0, _candidateAddress));
-        emit CandidateRegistered(_name);
-    }
-
-    function registerVoter(address _voter) public onlyOwner beforeRegistrationDeadline {
-        voters[_voter].registered = true;
-        emit VoterRegistered(_voter);
-    }
-
-    function castVote(address[] memory _preferences) public onlyRegisteredVoter beforeVotingDeadline {
-        require(!voters[msg.sender].voted, "You have already voted");
-        require(_preferences.length == candidates.length, "Invalid number of preferences");
-
-        uint256[] memory points = new uint256[](3);
-        points[0] = 5;
-        points[1] = 3;
-        points[2] = 1;
-
-        for (uint256 i = 0; i < _preferences.length; i++) {
-            require(voters[msg.sender].preferences.length < 3, "You can only vote for three candidates");
-            require(voters[msg.sender].preferences.length == i || _preferences[i] != voters[msg.sender].preferences[i], "Duplicate vote");
-
-            voters[msg.sender].preferences.push(_preferences[i]);
-            candidates[getIndex(_preferences[i])].voteCount += points[i];
-            totalVotes += points[i];
+    function regCandidate(string memory name) external {
+        address ID = msg.sender;
+        uint256 flag =0;
+        for(uint256 i =0; i< candidates.length;i++)
+        {
+            if(candidates[i].id == ID)
+            {
+                flag =1;
+                break;
+            }
         }
+        if(flag==0)
+        candidates.push(Candidate({name: name, score: 0, id:msg.sender}));
+    }
 
+    function isVoter() public view returns(bool isVoter_) {
+        if(voters[msg.sender].registered == true)
+        return true;
+        else 
+        return false;
+    }
+    function castVote(uint256[3] memory pref) external {
+        Voter storage voter = voters[msg.sender];
+        require(voter.registered == true,"Voter is not registered");
+        require(!voter.voted, "Already voted.");
+        candidates[pref[0]-1].score += 5;
+        candidates[pref[1]-1].score += 3;
+        candidates[pref[2]-1].score += 1;
         voters[msg.sender].voted = true;
-        emit VoteCast(msg.sender, _preferences);
-    }
-
-    function declareWinner() public onlyOwner {
-        require(block.timestamp >= votingDeadline, "Voting is still ongoing");
-
-        uint256 maxVoteCount = 0;
-        uint256 winnerIndex = 0;
-
-        for (uint256 i = 0; i < candidates.length; i++) {
-            if (candidates[i].voteCount > maxVoteCount) {
-                maxVoteCount = candidates[i].voteCount;
-                winnerIndex = i;
-            }
+        // Determine the winning candidate.
+    function winningCandidate() public view returns (uint256 winningCandidate_)
+    {
+        uint256 max = 0;
+        for(uint256 i =1; i< candidates.length; i++)
+        {
+           if(candidates[i].score> candidates[max].score)
+                 max = i;
         }
-
-        emit ElectionResult(candidates[winnerIndex].name, maxVoteCount);
+         require(!(candidates[max].score == 0), "No votes have been casted.");
+         return max;
+        // Return the index of the winner in `winningCandidate_`
+        // Error and revert if no votes have been cast yet.
     }
 
-    function getIndex(address _candidate) private view returns (uint256) {
-        for (uint256 i = 0; i < candidates.length; i++) {
-            if (candidates[i].candidateAddress == _candidate) {
-                return i;
-            }
-        }
-        revert("Candidate not found");
+    // Calls winningCandidate() function to get the index
+    // of the winner and then returns the name of the winner
+    function winningCandidateName() external view returns (string memory winnerName_)
+    {
+        require(msg.sender == leader, "Only leader can access the name of the winner");
+        uint256 index = winningCandidate();
+        return candidates[index].name;
     }
-}
+}}
